b881c50cda61356eb2798644e973aaa737711460
Henrik Sandklef <hesa@sandklef.com>
2024-12-05 23:53:35 +0100

    test version(s)

--- tests/shell/test-cli.sh
+++ tests/shell/test-cli.sh
@@ -4,6 +4,8 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+LT_VERSION=$(grep licomp_toolkit_version licomp_toolkit/config.py | cut -d = -f 2 | sed "s,[' ]*,,g")
+
 if [ "$1" == "--local" ]
 then
     IMPLEMENTATIONS=../licomp:../licomp-dwheeler:../licomp-hermione:../licomp-osadl:../licomp-reclicense:../licomp-proprietary:.
@@ -34,6 +36,33 @@ test_licomp_tk() {
     fi
     echo OK
 }
+
+test_licomp_tk_text() {
+    COMMAND="$1"
+    EXTRA_COMMAND="$2"
+    EXP="$3"
+
+    echo -n "$COMMAND: "
+    ACTUAL=$(echo PYTHONPATH=$IMPLEMENTATIONS:. ./licomp_toolkit/__main__.py $COMMAND $EXTRA_COMMAND | bash)
+
+    if [ "$EXP" != "$ACTUAL" ]
+    then
+	echo "Fail"
+	echo "  expected: $EXP"
+	echo "  actual:   $ACTUAL"
+	echo "  command:  $COMMAND"
+	echo "  extra:    $EXTRA_COMMAND"
+        echo "  command:  devel/licomp-toolkit $COMMAND"
+	exit 1
+    fi
+    echo OK
+}
+
+echo "# test version"
+test_licomp_tk_text "--version" "" "$LT_VERSION"
+test_licomp_tk_text "--name" "" licomp-toolkit
+test_licomp_tk_text "versions" "| head -1" "licomp-toolkit:$LT_VERSION"
+test_licomp_tk_text "versions" "| wc -l" "6"
 
 echo "# test supported/unsupported licenses"
 test_licomp_tk "verify -il MIT -ol MIT" ".summary.results.yes.count" 3
