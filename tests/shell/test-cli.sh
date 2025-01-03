2538a403ec4443e14f2ebbc2655d3c0e13e585a1
Henrik Sandklef <hesa@sandklef.com>
2025-01-03 08:40:18 +0100

    adjust tests to one more resource

--- tests/shell/test-cli.sh
+++ tests/shell/test-cli.sh
@@ -8,7 +8,7 @@ LT_VERSION=$(grep licomp_toolkit_version licomp_toolkit/config.py | cut -d = -f 
 
 if [ "$1" == "--local" ]
 then
-    IMPLEMENTATIONS=../licomp:../licomp-dwheeler:../licomp-hermione:../licomp-osadl:../licomp-reclicense:../licomp-proprietary:.
+    IMPLEMENTATIONS=../licomp:../licomp-dwheeler:../licomp-hermione:../licomp-osadl:../licomp-reclicense:../licomp-proprietary::../licomp-gnuguide:.
     shift
 fi
 
@@ -64,13 +64,13 @@ test_version()
     test_licomp_tk_text "--version" "" "$LT_VERSION"
     test_licomp_tk_text "--name" "" licomp-toolkit
     test_licomp_tk_text "versions" "| head -1" "licomp-toolkit:$LT_VERSION"
-    test_licomp_tk_text "versions" "| wc -l" "6"
+    test_licomp_tk_text "versions" "| wc -l" "7"
 }
 
 test_supp_unsupp()
 {
     echo "# test supported/unsupported licenses"
-    test_licomp_tk "verify -il MIT -ol MIT" ".summary.results.yes.count" 3
+    test_licomp_tk "verify -il MIT -ol MIT" ".summary.results.yes.count" 4
     test_licomp_tk "verify -il MIT -ol MIT2" ".summary.results.nr_valid" 0
     test_licomp_tk "verify -il MIT2 -ol MIT" ".summary.results.nr_valid" 0
     test_licomp_tk "verify -il MIT2 -ol MIT2" ".summary.results.nr_valid" 0
@@ -92,22 +92,22 @@ test_snippet_bindist()
     echo "# snippet vs bin dist"
     test_licomp_tk "-u snippet verify -il BSD-3-Clause -ol LGPL-2.1-or-later" ".summary.results.nr_valid" 2
     test_licomp_tk "-u snippet verify -il BSD-3-Clause -ol LGPL-2.1-or-later" ".summary.results.yes.count" 2
-    test_licomp_tk "verify -il BSD-3-Clause -ol LGPL-2.1-or-later" ".summary.results.nr_valid" 2
-    test_licomp_tk "verify -il BSD-3-Clause -ol LGPL-2.1-or-later" ".summary.results.yes.count" 2
+    test_licomp_tk "verify -il BSD-3-Clause -ol LGPL-2.1-or-later" ".summary.results.nr_valid" 3
+    test_licomp_tk "verify -il BSD-3-Clause -ol LGPL-2.1-or-later" ".summary.results.yes.count" 3
 }
 
 test_supports_license()
 {
     echo "# supports license"
-    test_licomp_tk_text " -of text supports-license MIT" " | wc -l" 5
-    test_licomp_tk_text " -of json supports-license MIT" " | jq .[] | wc -l" 5
-    test_licomp_tk_text " supports-license MIT" " | jq .[] | wc -l" 5
+    test_licomp_tk_text " -of text supports-license MIT" " | wc -l" 6
+    test_licomp_tk_text " -of json supports-license MIT" " | jq .[] | wc -l" 6
+    test_licomp_tk_text " supports-license MIT" " | jq .[] | wc -l" 6
 }
 
 test_supports_provisioning()
 {
     echo "# supports provisioning"
-    test_licomp_tk_text " -of text supports-provisioning binary-distribution" " | wc -l" 5
+    test_licomp_tk_text " -of text supports-provisioning binary-distribution" " | wc -l" 6
     test_licomp_tk_text " -of text supports-provisioning provide-webui" " | grep -v \"^$\" | wc -l " 0
 }
 
@@ -115,7 +115,7 @@ test_supports_usecase()
 {
     echo "# supports usecase"
     test_licomp_tk_text " -of text supports-usecase snippet" " | wc -l" 2
-    test_licomp_tk_text " -of text supports-usecase library" " | wc -l " 3
+    test_licomp_tk_text " -of text supports-usecase library" " | wc -l " 4
 }
 
 
