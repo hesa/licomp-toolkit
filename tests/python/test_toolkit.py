2966d1829eeda987f677d5205989ab4c79a5e61d
Henrik Sandklef <hesa@sandklef.com>
2025-01-24 00:26:18 +0100

    update test with new licomp resource and return codes

--- tests/python/test_toolkit.py
+++ tests/python/test_toolkit.py
@@ -32,7 +32,8 @@ def test_provisioning_is_not_supported():
 def test_compat():
     ret = lt.outbound_inbound_compatibility("GPL-2.0-only", "BSD-3-Clause", UseCase.usecase_to_string(UseCase.LIBRARY), Provisioning.provisioning_to_string(Provisioning.BIN_DIST))
     logging.debug("ret: " + str(ret['summary']['results']))
-    assert ret['summary']['results']['yes']['count'] == 3
+    print("... hesa: " + str(ret['summary']['results']['yes']))
+    assert ret['summary']['results']['yes']['count'] == 4
 
 def test_incompat():
     ret = lt.outbound_inbound_compatibility("BSD-3-Clause", "GPL-2.0-only",  UseCase.usecase_to_string(UseCase.LIBRARY), Provisioning.provisioning_to_string(Provisioning.BIN_DIST))
