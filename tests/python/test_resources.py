83b31cb50236a8a0bdce16a5a9b5f823ed6b471e
Henrik Sandklef <hesa@sandklef.com>
2025-12-22 00:05:25 +0100

    add tests for standard/optional resources

--- tests/python/test_resources.py
+++ tests/python/test_resources.py
@@ -14,11 +14,13 @@ from licomp_toolkit.toolkit import LicompToolkit
 
 lt = LicompToolkit()
 resource_names = [x for x in lt.licomp_resources()]
+standard_resource_names = [x for x in lt.licomp_standard_resources()]
+optional_resource_names = [x for x in lt.licomp_optional_resources()]
 
 def test_supported_resources():
     assert len(lt.licomp_resources()) == 6
     
-def test_supported_resources_reclicense():
+def test_supported_resources():
     assert "licomp_reclicense" in resource_names
     assert "licomp_osadl" in resource_names
     assert "licomp_proprietary" in resource_names
@@ -26,3 +28,18 @@ def test_supported_resources_reclicense():
     assert "licomp_dwheeler" in resource_names
     assert "licomp_gnuguide" in resource_names
 
+def test_supported_resources_standard():
+    assert "licomp_reclicense" in standard_resource_names
+    assert "licomp_osadl" in standard_resource_names
+    assert "licomp_proprietary" not in standard_resource_names
+    assert "licomp_hermione" not in standard_resource_names
+    assert "licomp_dwheeler" not in standard_resource_names
+    assert "licomp_gnuguide" not in standard_resource_names
+
+def test_supported_resources_standard():
+    assert "licomp_reclicense" not in optional_resource_names
+    assert "licomp_osadl" not in optional_resource_names
+    assert "licomp_proprietary" in optional_resource_names
+    assert "licomp_hermione" in optional_resource_names
+    assert "licomp_dwheeler" in optional_resource_names
+    assert "licomp_gnuguide" in optional_resource_names
