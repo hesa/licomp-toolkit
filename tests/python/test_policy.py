a46fc9b0709cb1f7b69f072a11ec34d2c905506b
Henrik Sandklef <hesa@sandklef.com>
2026-01-12 21:02:45 +0100

    add more policy tests

--- tests/python/test_policy.py
+++ tests/python/test_policy.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2025 Henrik Sandklef
+# SPDX-FileCopyrightText: 2026 Henrik Sandklef
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -9,6 +9,7 @@ import sys
 
 from licomp_toolkit.license_policy import LicensePolicy
 from licomp_toolkit.license_policy import DefaultLicensePolicy
+from licomp_toolkit.license_policy import LicensePolicyException
 from licomp_toolkit.license_policy import LicensePolicyHandler
 from licomp_toolkit.toolkit import ExpressionExpressionChecker
 from licomp.interface import UseCase
@@ -20,6 +21,8 @@ policy = LicensePolicy(TEST_POLICY_FILE)
 policy_handler = LicensePolicyHandler(TEST_POLICY_FILE)
 expr_checker = ExpressionExpressionChecker()
 
+default_policy = DefaultLicensePolicy(['licomp_reclicense'], 'library', 'binary-distribution')
+
 def test_policy_allowed():
     assert "MIT" in policy.allowed()
     assert "BSD-3-Clause" in policy.allowed()
@@ -27,13 +30,13 @@ def test_policy_allowed():
     assert "BSD-4-Clause" not in policy.allowed()
     assert "BSD-2-Clause-Patent" not in policy.allowed()
     
-def test_policy_avoid():
-    assert "MIT" not in policy.avoid()
-    assert "BSD-3-Clause" not in policy.avoid()
+def test_policy_avoided():
+    assert "MIT" not in policy.avoided()
+    assert "BSD-3-Clause" not in policy.avoided()
 
-    assert "BSD-4-Clause"  in policy.avoid()
+    assert "BSD-4-Clause"  in policy.avoided()
     
-    assert "BSD-2-Clause-Patent" not in policy.avoid()
+    assert "BSD-2-Clause-Patent" not in policy.avoided()
     
 def test_policy_denied():
     assert "MIT" not in policy.denied()
@@ -54,6 +57,42 @@ def _test_expr_expr_library_bin(outbound, inbound, policy_file=None):
     return compats
     
 
+def test_policy_preferences_allowed():
+    assert policy.compare_preferences('MIT', 'BSD-3-Clause') < 0
+    assert policy.compare_preferences('BSD-3-Clause', 'MIT') > 0
+    assert policy.compare_preferences('BSD-3-Clause', 'BSD-3-Clause') == 0
+    
+def test_policy_preferences_allowed_avoided():
+    assert policy.compare_preferences('MIT', 'BSD-4-Clause') < 0
+    assert policy.compare_preferences('BSD-4-Clause', 'MIT') > 0
+    assert policy.compare_preferences('BSD-4-Clause', 'BSD-4-Clause') == 0
+    
+def test_policy_preferences_allowed_denied():
+    assert policy.compare_preferences('MIT', 'BSD-2-Clause-Patent') < 0
+    assert policy.compare_preferences('BSD-2-Clause-Patent', 'MIT') > 0
+    assert policy.compare_preferences('BSD-2-Clause-Patent', 'BSD-2-Clause-Patent') == None
+    
+def test_policy_preferences_allowed_denied_ignore():
+    assert policy.compare_preferences('MIT', 'BSD-2-Clause-Patent', ignore_missing=True) < 0
+    assert policy.compare_preferences('BSD-2-Clause-Patent', 'BSD-2-Clause-Patent', ignore_missing=True) == None
+
+def test_policy_preferences_allowed_denied_names():
+    assert policy.most_preferred('MIT', 'BSD-3-Clause') == 'MIT'
+    assert policy.most_preferred('MIT', 'BSD-2-Clause-Patent') == 'MIT'
+    assert policy.most_preferred('BSD-2-Clause-Patent', 'BSD-3-Clause') == 'BSD-3-Clause'
+    assert policy.most_preferred('BSD-2-Clause-Patent', 'BSD-2-Clause-Patent') == None
+    
+def test_policy_preferences_allowed_denied_names_ignore():
+    assert policy.most_preferred('BSD-2-Clause-Patent', 'BSD-3-Clause', ignore_missing=True) == 'BSD-3-Clause'
+    assert policy.most_preferred('BSD-2-Clause-Patent', 'BSD-2-Clause-Patent', ignore_missing=True) == None
+
+def test_policy_preferences_raises():
+    with pytest.raises(LicensePolicyException) as e_info:
+        policy.most_preferred('MIT2', 'GPL-2.0-or-later2')
+
+def test_policy_preferences_raises_ignore():
+    policy.most_preferred('MIT2', 'GPL-2.0-or-later2', ignore_missing=True) == None
+
 # BRING BACK - RENAME
 def _test_expr_expr_1(): 
     compat_report = _test_expr_expr_library_bin('MIT OR 0BSD', 'MIT OR (BSD-3-Clause AND MIT)')
@@ -64,7 +103,27 @@ def _test_expr_expr_1():
     assert False
 
 
-def test_default_policy():
-    dpol = DefaultLicensePolicy(['licomp_reclicense'], 'library', 'binary-distribution')
+def test_default_policy_listed():
+    assert "MIT" in default_policy.allowed()
+    assert "BSD-3-Clause" in default_policy.allowed()
 
-    assert False
+    assert "BSD-4-Clause" in default_policy.allowed()
+    assert "BSD-2-Clause-Patent" in default_policy.allowed()
+
+    assert len(default_policy.avoided()) == 0
+    assert len(default_policy.denied()) == 0
+
+def test_default_policy_preference():
+    assert default_policy.compare_preferences('MIT', 'GPL-2.0-or-later') < 0
+    assert default_policy.compare_preferences('GPL-2.0-or-later', 'MIT') > 0
+    assert default_policy.compare_preferences('MIT', 'MIT') == 0
+
+def test_default_policy_preferences_allowed_denied_names():
+    assert default_policy.most_preferred('MIT', 'BSD-3-Clause') == 'MIT'
+    assert default_policy.most_preferred('MIT', 'GPL-2.0-or-later') == 'MIT'
+
+    
+def test_default_policy_preferences_raises():
+    with pytest.raises(LicensePolicyException) as e_info:
+        default_policy.most_preferred('MIT2', 'GPL-2.0-or-later2')
+    
