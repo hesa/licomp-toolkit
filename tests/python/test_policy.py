bf80c19385353b6e7fa8d55c2f5a144a2a380169
Henrik Sandklef <hesa@sandklef.com>
2026-02-16 18:09:10 +0100

    adjust to new format

--- tests/python/test_policy.py
+++ tests/python/test_policy.py
@@ -19,9 +19,17 @@ TEST_POLICY_FILE = 'tests/policy/license-policy.json'
 
 policy = LicensePolicy(TEST_POLICY_FILE)
 policy_handler = LicensePolicyHandler(TEST_POLICY_FILE)
+default_policy_handler = LicensePolicyHandler(
+    resources = ['licomp_reclicense'],
+    usecase = 'library',
+    provisioning = 'binary-distribution')
 expr_checker = ExpressionExpressionChecker()
 
-default_policy = DefaultLicensePolicy(['licomp_reclicense'], 'library', 'binary-distribution')
+default_policy = DefaultLicensePolicy(
+    ['licomp_reclicense'],
+    'library',
+    'binary-distribution')
+    
 
 def test_policy_allowed():
     assert "MIT" in policy.allowed()
@@ -68,6 +76,7 @@ def test_policy_preferences_allowed_avoided():
     assert policy.compare_preferences('BSD-4-Clause', 'BSD-4-Clause') == 0
     
 def test_policy_preferences_allowed_denied():
+    assert policy.compare_preferences('BSD-3-Clause', 'BSD-3-Clause') == 0
     assert policy.compare_preferences('MIT', 'BSD-2-Clause-Patent') < 0
     assert policy.compare_preferences('BSD-2-Clause-Patent', 'MIT') > 0
     assert policy.compare_preferences('BSD-2-Clause-Patent', 'BSD-2-Clause-Patent') == None
@@ -86,9 +95,29 @@ def test_policy_preferences_allowed_denied_names_ignore():
     assert policy.most_preferred('BSD-2-Clause-Patent', 'BSD-3-Clause', ignore_missing=True) == 'BSD-3-Clause'
     assert policy.most_preferred('BSD-2-Clause-Patent', 'BSD-2-Clause-Patent', ignore_missing=True) == None
 
+def OBSOLETE_test_policy_preferred_score_ignore_missing():
+    assert policy.preferred_score_ignore_missing('MIT', 'BSD-3-Clause') == -1
+    assert policy.preferred_score_ignore_missing('BSD-3-Clause', 'BSD-3-Clause') == 0
+    assert policy.preferred_score_ignore_missing('BSD-3-Clause', 'MIT') == 1
+
+def OBSOLETE_test_policy_sorted():
+    assert policy.preferred_score_ignore_missing('MIT', 'BSD-3-Clause') == -1
+    assert policy.preferred_score_ignore_missing('BSD-3-Clause', 'BSD-3-Clause') == 0
+    assert policy.preferred_score_ignore_missing('BSD-3-Clause', 'MIT') == 1
+    assert policy.preferred_score_ignore_missing('BSD-3-Clause', 'GPL-2.0-or-later') == -1
+    from functools import cmp_to_key
+    inbounds = ['MIT', 'GPL-2.0-or-later', 'BSD-3-Clause']
+    sorted_inbounds = sorted(inbounds, key=cmp_to_key(policy.preferred_score_ignore_missing))
+    print("TEST: " + str(inbounds))
+    print("TEST: " + str(sorted_inbounds))
+    assert sorted_inbounds == ['MIT', 'BSD-3-Clause', 'GPL-2.0-or-later']
+    
+
 def test_policy_preferences_raises():
     with pytest.raises(LicensePolicyException) as e_info:
         policy.most_preferred('MIT2', 'GPL-2.0-or-later2')
+    with pytest.raises(LicensePolicyException) as e_info:
+        policy.most_preferred('MIT2', 'GPL-2.0-or-later2', ignore_missing=False)
 
 def test_policy_preferences_raises_ignore():
     policy.most_preferred('MIT2', 'GPL-2.0-or-later2', ignore_missing=True) == None
@@ -119,6 +148,7 @@ def test_default_policy_preference():
     assert default_policy.compare_preferences('MIT', 'MIT') == 0
 
 def test_default_policy_preferences_allowed_denied_names():
+    assert default_policy.most_preferred('BSD-3-Clause', 'BSD-3-Clause') == 'BSD-3-Clause'
     assert default_policy.most_preferred('MIT', 'BSD-3-Clause') == 'MIT'
     assert default_policy.most_preferred('MIT', 'GPL-2.0-or-later') == 'MIT'
 
@@ -127,3 +157,134 @@ def test_default_policy_preferences_raises():
     with pytest.raises(LicensePolicyException) as e_info:
         default_policy.most_preferred('MIT2', 'GPL-2.0-or-later2')
     
+def test_preferred_score_inbounds():
+    OBSD_MIT = {'outbound': {'license': 'MIT', 'type': 'license', 'preferences': {'license': 'MIT', 'license_list': 'allowed', 'license_index': 3}}, 'inbound': {'license': '0BSD', 'type': 'license', 'preferences': {'license': '0BSD', 'license_list': 'allowed', 'license_index': 2}}, 'compatibility': 'yes', 'unusable': {'unusable': []}}
+    #{'check_type': 'inbound', 'inbound_license': '0BSD', 'outbound_license': 'MIT', 'inbound_license_type': 'license', 'outbound_license_type': 'license', 'compatibility': 'yes', 'inbound_list': 'allowed', 'inbound_list_index': 3}
+    
+    ISC_MIT = {'outbound': {'license': 'MIT', 'type': 'license', 'preferences': {'license': 'MIT', 'license_list': 'allowed', 'license_index': 3}}, 'inbound': {'license': 'ISC', 'type': 'license', 'preferences': {'license': 'ISC', 'license_list': 'allowed', 'license_index': 4}}, 'compatibility': 'yes', 'unusable': {'unusable': []}}
+    #{'check_type': 'inbound', 'inbound_license': 'ISC', 'outbound_license': 'MIT', 'inbound_license_type': 'license', 'outbound_license_type': 'license', 'compatibility': 'yes', 'inbound_list': 'allowed', 'inbound_list_index': 7}
+
+    assert default_policy.preferred_score_inbounds(OBSD_MIT, ISC_MIT) < 0
+    assert default_policy.preferred_score_inbounds(ISC_MIT, OBSD_MIT) > 0
+    assert default_policy.preferred_score_inbounds(ISC_MIT, ISC_MIT) == 0
+        
+def test_scored_inbounds_zerobsd_isc():
+    inbounds =  [
+        {
+            "outbound": {
+                "license": "MIT",
+                "type": "license",
+                "preferences": {
+                    "license": "MIT",
+                    "license_list": "allowed",
+                    "license_index": 0
+                }
+            },
+            "inbound": {
+                "license": "0BSD",
+                "type": "license",
+                "preferences": {
+                    "license": "0BSD",
+                    "license_list": "allowed",
+                    "license_index": 4
+                }
+            },
+            "compatibility": "yes",
+            "unusable": []
+        },
+        {
+            "outbound": {
+                "license": "MIT",
+                "type": "license",
+                "preferences": {
+                    "license": "MIT",
+                    "license_list": "allowed",
+                    "license_index": 0
+                }
+            },
+            "inbound": {
+                "license": "ISC",
+                "type": "license",
+                "preferences": {
+                    "license": "ISC",
+                    "license_list": "",
+                    "license_index": None
+                }
+            },
+            "compatibility": "yes",
+            "unusable": []
+        }
+    ]
+
+    scored_inbounds, unusable = default_policy_handler.scored_inbounds(inbounds, "OR")
+    assert len(scored_inbounds) == 2
+    assert len(scored_inbounds[0]['inbound']) == 3
+    assert scored_inbounds[0]['inbound']['license'] == '0BSD'
+    assert scored_inbounds[1]['inbound']['license'] == 'ISC'
+
+        
+def test_scored_inbounds_isc_zerobsd():
+    inbounds = [
+        {
+            "outbound": {
+                "license": "MIT",
+                "type": "license",
+                "preferences": {
+                    "license": "MIT",
+                    "license_list": "allowed",
+                    "license_index": 0
+                }
+            },
+            "inbound": {
+                "license": "ISC",
+                "type": "license",
+                "preferences": {
+                    "license": "ISC",
+                    "license_list": "",
+                    "license_index": None
+                }
+            },
+            "compatibility": "yes",
+            "unusable": []
+        },
+        {
+            "outbound": {
+                "license": "MIT",
+                "type": "license",
+                "preferences": {
+                    "license": "MIT",
+                    "license_list": "allowed",
+                    "license_index": 0
+                }
+            },
+            "inbound": {
+                "license": "0BSD",
+                "type": "license",
+                "preferences": {
+                    "license": "0BSD",
+                    "license_list": "allowed",
+                    "license_index": 4
+                }
+            },
+            "compatibility": "yes",
+            "unusable": []
+        }
+    ]
+
+    scored_inbounds, unusable = default_policy_handler.scored_inbounds(inbounds, "OR")
+    assert len(scored_inbounds) == 2
+    assert len(scored_inbounds[0]['inbound']) == 3
+    assert scored_inbounds[0]['inbound']['license'] == '0BSD'
+    assert scored_inbounds[1]['inbound']['license'] == 'ISC'
+
+    
+def test_usable_license():
+    LIC = {'outbound': {'license': 'MIT', 'type': 'license', 'preferences': {'license': 'MIT', 'license_list': 'allowed', 'license_index': 3}}, 'inbound': {'license': 'BSD-3-Clause', 'type': 'license', 'preferences': {'license': 'BSD-3-Clause', 'license_list': 'allowed', 'license_index': 2}}, 'compatibility': 'no', 'unusable': {'unusable': []}}
+    
+    assert not policy_handler.usable_license(LIC, 'inbound')
+    assert not policy_handler.usable_license(LIC, 'outbound')
+
+    LIC['compatibility'] = 'yes'
+    assert policy_handler.usable_license(LIC, 'inbound')
+    assert policy_handler.usable_license(LIC, 'outbound')
+
