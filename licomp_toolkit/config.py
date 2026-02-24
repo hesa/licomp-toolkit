56068014a6f4e7f5800fc8a11ab137ff27063163
hesa <hesa@localhost>
2026-02-24 23:05:49 +0000

    Release 0.5.20

--- licomp_toolkit/config.py
+++ licomp_toolkit/config.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-licomp_toolkit_version = '0.5.19'
+licomp_toolkit_version = '0.5.20'
 my_supported_api_version = '0.5'
 licomp_toolkit_file_version = '0.5'
 
